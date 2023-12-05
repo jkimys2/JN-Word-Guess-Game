d1a2350f32f49ec5ec2325b39fac8d6d1fcfa8ad
Jordan <jkimys2@gmail.com>
2023-12-04 20:45:43 -0500

    initial push

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// querySelect all of the elements
+var 
+
+
+
+// want to initiate game when pressing start
+
+
+
+
+// start timer once game is initiated
+
+
+
+
+// store words and show letters when keydown event happens
+
+
+
+
+// if statement: if user wins display you win and increase win score; if user loses display you lose and increase lose score
+
+
+
+
+// reset score if user clicks reset
